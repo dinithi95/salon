7706f27af732d6bba751a28f3dafae7e37c3f51f
sameera995 <samdilshan995@gmail.com>
2020-09-03 11:25:11 +0530

    add table

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -1,6 +1,12 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {NzFormatEmitEvent, NzTreeComponent, NzTreeNodeOptions} from 'ng-zorro-antd';
+import {Service} from "../service/Service";
+
+// interface service {
+//   name: string;
+//   price: string;
+//   time:
+// }
 
 @Component({
   selector: 'app-appointment',
@@ -9,45 +15,152 @@ import {NzFormatEmitEvent, NzTreeComponent, NzTreeNodeOptions} from 'ng-zorro-an
 })
 export class AppointmentComponent implements OnInit {
   appointmentForm: FormGroup;
-  radioValue = 'Married';
-  @ViewChild('nzTreeComponent', {static: false}) nzTreeComponent!: NzTreeComponent;
-  defaultCheckedKeys = ['10020'];
-  defaultSelectedKeys = ['10010'];
-  defaultExpandedKeys = ['100', '1001'];
+  hair: Service[] = [];
+  bridal: Service[] = [];
+  beauty: Service[] = [];
+  checkedServices: Service[] = [];
+  current = 0;
 
-  nodes: NzTreeNodeOptions[] = [
+  services: Service[] = [
     {
-      title: 'parent 1',
-      key: '100',
-      children: [
-        {
-          title: 'parent 1-0',
-          key: '1001',
-          disabled: true,
-          children: [
-            {title: 'leaf 1-0-0', key: '10010', disableCheckbox: true, isLeaf: true},
-            {title: 'leaf 1-0-1', key: '10011', isLeaf: true}
-          ]
-        },
-        {
-          title: 'parent 1-1',
-          key: '1002',
-          children: [
-            {title: 'leaf 1-1-0', key: '10020', isLeaf: true},
-            {title: 'leaf 1-1-1', key: '10021', isLeaf: true}
-          ]
+      id: '1',
+      code: '0010',
+      name: 'Layers',
+      price: '1000.00',
+      time: '30.00',
+      subCategory: {
+        code: '001',
+        name: 'Cutting',
+        category: {
+          code: '001',
+          name: 'Hair',
         }
-      ]
-    }
-  ];
+      }
+    },
+    {
+      id: '2',
+      code: '0011',
+      name: 'V shape',
+      price: '1000.00',
+      time: '30.00',
+      subCategory: {
+        code: '001',
+        name: 'Cutting',
+        category: {
+          code: '001',
+          name: 'Hair',
+        }
+      }
+    },
+    {
+      id: '3',
+      code: '0012',
+      name: 'Short',
+      price: '1000.00',
+      time: '30.00',
+      subCategory: {
+        code: '001',
+        name: 'Cutting',
+        category: {
+          code: '001',
+          name: 'Hair',
+        }
+      }
+    },
+    {
+      id: '4',
+      code: '0010',
+      name: 'Black',
+      price: '2000.00',
+      time: '30.00',
+      subCategory: {
+        code: '002',
+        name: 'Coloring',
+        category: {
+          code: '001',
+          name: 'Hair',
+        }
+      }
+    },
+    {
+      id: '5',
+      code: '0011',
+      name: 'Brown',
+      price: '3000.00',
+      time: '30.00',
+      subCategory: {
+        code: '002',
+        name: 'Coloring',
+        category: {
+          code: '001',
+          name: 'Hair',
+        }
+      }
+    },
+    {
+      id: '6',
+      code: '0013',
+      name: 'Platinum',
+      price: '300000.00',
+      time: '600.00',
+      subCategory: {
+        code: '001',
+        name: 'Dressing',
+        category: {
+          code: '001',
+          name: 'Bridal',
+        }
+      }
+    },
+    {
+      id: '7',
+      code: '0014',
+      name: 'Gold',
+      price: '200000.00',
+      time: '600.00',
+      subCategory: {
+        code: '001',
+        name: 'Dressing',
+        category: {
+          code: '001',
+          name: 'Bridal',
+        }
+      }
+    },
+    {
+      id: '8',
+      code: '0015',
+      name: 'Silver',
+      price: '100000.00',
+      time: '600.00',
+      subCategory: {
+        code: '001',
+        name: 'Dressing',
+        category: {
+          code: '001',
+          name: 'Bridal',
+        }
+      }
+    },
+  ]
 
   constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.formControl();
+    this.services.map((service) => {
+      if (service.subCategory.category.name === 'Hair') {
+        this.hair.push(service);
+      }
+      if (service.subCategory.category.name === 'Bridal') {
+        this.bridal.push(service);
+      }
+      if (service.subCategory.category.name === 'Beauty') {
+        this.beauty.push(service);
+      }
+    });
   }
-
 
 
   formControl() {
@@ -72,29 +185,24 @@ export class AppointmentComponent implements OnInit {
   }
 
 
-  nzClick(event: NzFormatEmitEvent): void {
-    console.log(event);
+  updateCheckedSet(service: Service, checked: boolean): void {
+    if (checked) {
+      this.checkedServices.push(service);
+    } else {
+      let index = this.checkedServices.indexOf(service);
+      if (index !== -1) { this.checkedServices.splice(index, 1); }
+    }
   }
 
-  nzCheck(event: NzFormatEmitEvent): void {
-    console.log(event);
-  }
-
-  // nzSelectedKeys change
-  // nzSelect(keys: string[]): void {
-  //   console.log(keys, this.nzTreeComponent.getSelectedNodeList());
+  // refreshCheckedStatus(): void {
+  //   this.checked = this.listOfCurrentPageData.every(item => this.checkedServices.indexOf(item));
+  //   this.indeterminate = this.listOfCurrentPageData.some(item => this.checkedServices.indexOf(item)) && !this.checked;
   // }
 
-  ngAfterViewInit(): void {
-    console.log(this.nzTreeComponent.getTreeNodeByKey('10011'));
-    // use tree methods
-    console.log(
-      this.nzTreeComponent.getTreeNodes(),
-      this.nzTreeComponent.getCheckedNodeList(),
-      this.nzTreeComponent.getSelectedNodeList(),
-      this.nzTreeComponent.getExpandedNodeList()
-    );
+  onItemChecked(service: Service, checked: boolean): void {
+    this.updateCheckedSet(service, checked);
+    // this.refreshCheckedStatus();
+    console.log(this.checkedServices);
   }
-
 
 }
