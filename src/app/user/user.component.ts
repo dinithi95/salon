365613ab5089810bb0336f09fab64ad7db11037a
sameera995 <samdilshan995@gmail.com>
2020-10-31 11:13:45 +0530

    usr

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
+import {User} from "./User";
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-user',
@@ -9,31 +12,68 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class UserComponent implements OnInit {
 
   userForm: FormGroup;
+  passwordVisible = false;
+  searchValue = '';
+  visible = false;
+  users: User[] = [];
+  displayUsers = [...this.users];
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private http: HttpClient,
+              private userService: UserService) {
   }
 
   ngOnInit(): void {
     this.formControl();
+    this.getAllUsers();
   }
 
   formControl() {
     this.userForm = this.fb.group({
       id: null,
-      code: ['', [Validators.required]],
       name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      nic: ['', [Validators.required]],
-      gender: ['', [Validators.required]]
+      password: ['', [Validators.required]],
+      password_confirmation: ['', [Validators.required, this.confirmationValidator]]
+    });
+  }
+
+  getAllUsers() {
+    this.userService.getAllUsers().subscribe(value => {
+      this.users = value;
+      this.displayUsers = value;
     });
   }
 
   submitForm() {
-// need to create laravel backend before write this code(insert part)
+    this.http.put(`http://localhost:8000/api/register`, this.userForm.value).subscribe();
   }
 
   resetForm(e: MouseEvent): void {
     this.userForm.reset();
+  }
+
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return {required: true};
+    } else if (control.value !== this.userForm.controls.password.value) {
+      return {confirm: true, error: true};
+    }
+    return {};
+  }
+
+  resetSearch(): void {
+    this.searchValue = '';
+    this.search();
+  }
+
+  search(): void {
+    this.visible = false;
+    this.displayUsers = this.users.filter((item: User) => item.name.indexOf(this.searchValue) !== -1);
+  }
+
+  fillForm(data: User) {
+
   }
 
 }
