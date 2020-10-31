d2f44b8af7a0ab59d8a3ca647f3e0f8f008fe71f
Dinithi95 <dinithikasathsarani20@gmail.com>
2020-10-31 14:19:17 +0530

    changes

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-service',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
+  serviceForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formControl();
   }
 
+  formControl() {
+    this.serviceForm = this.fb.group({
+      id: null,
+      code: ['', [Validators.required]],
+      catname: ['', [Validators.required]],
+      subname: ['', [Validators.required]],
+      servicecode: ['', [Validators.required]],
+      servicename: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      duration: ['', [Validators.required]]
+    });
+  }
+
+ // submitForm() {
+// need to create laravel backend before write this code(insert part)
+ // }
+
+  resetForm(e: MouseEvent): void {
+    this.serviceForm.reset();
+  }
 }
+
